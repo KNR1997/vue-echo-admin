bd80c8e055074d3f9a84606418fd3f1045e2a18c
KNR1997 <kethakaranasinghe@gmail.com>
2026-06-19 23:52:59 +0530

    feat: onSuccess message depends on response

--- src/data/menu.ts
+++ src/data/menu.ts
@@ -1,109 +1,108 @@
-import { computed } from 'vue'
-import { useMessage } from 'naive-ui'
-import { computedAsync } from '@vueuse/core'
-import { menuClient } from './client/menu'
-import { mapPaginatorData } from '@/utils/data-mappers'
-import { useMutation, useQuery, useQueryClient } from '@tanstack/vue-query'
-import type { Menu, MenuPaginator, MenuQueryOptions } from '@/types'
-import { API_ENDPOINTS } from './client/api-endpoints'
-import { addDynamicRoutes } from '@/router'
+import { computed } from "vue";
+import { useMessage } from "naive-ui";
+import { addDynamicRoutes } from "@/router";
+import { computedAsync } from "@vueuse/core";
+import { API_ENDPOINTS } from "./client/api-endpoints";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/vue-query";
+// clients
+import { menuClient } from "./client/menu";
+// utils
+import { mapPaginatorData } from "@/utils/data-mappers";
+// types
+import type { Menu, MenuPaginator, MenuQueryOptions } from "@/types";
 
 export const useMenusQuery = (options: Partial<MenuQueryOptions>) => {
   const { data, error, isPending } = useQuery<MenuPaginator, Error>({
     queryKey: [API_ENDPOINTS.MENUS, options],
     queryFn: ({ queryKey, pageParam }) =>
       menuClient.paginated(Object.assign({}, queryKey[1], pageParam)),
-  })
-  const menus = computedAsync<Menu[]>(() => data.value?.data ?? []) // todo -> fix
-  const paginationInfo = computed(() => mapPaginatorData(data.value))
+  });
+  const menus = computedAsync<Menu[]>(() => data.value?.data ?? []); // todo -> fix
+  const paginationInfo = computed(() => mapPaginatorData(data.value));
   return {
     menus,
     paginationInfo,
     error,
     loading: isPending,
-  }
-}
+  };
+};
 
 export const useCreateMenuMutation = () => {
-  const queryClient = useQueryClient()
-  const message = useMessage()
+  const queryClient = useQueryClient();
+  const message = useMessage();
 
   return useMutation({
     mutationFn: menuClient.create,
 
-    onSuccess: () => {
-      message.success('Created successfully')
+    onSuccess: (response) => {
+      message.success(response?.message ?? "Created successfully");
 
       queryClient.invalidateQueries({
         queryKey: [API_ENDPOINTS.MENUS],
-      })
+      });
     },
     onError: (error: Error) => {
-      console.error('Create Menu failed:', error)
-      message.error(error.message)
+      message.error(error.message);
     },
-  })
-}
+  });
+};
 
 export const useUpdateMenuMutation = () => {
-  const queryClient = useQueryClient()
-  const message = useMessage()
+  const queryClient = useQueryClient();
+  const message = useMessage();
 
   return useMutation({
     mutationFn: menuClient.update,
-    onSuccess: async () => {
-      message.success('Updated successfully')
+    onSuccess: async (response) => {
+      message.success(response?.message ?? "Updated successfully");
 
       queryClient.invalidateQueries({
         queryKey: [API_ENDPOINTS.MENUS],
-      })
+      });
 
       // reset dynamic routes
-      await addDynamicRoutes()
+      await addDynamicRoutes();
     },
     onError: (error: Error) => {
-      console.error('Update Menu failed:', error)
-      message.error(error.message)
+      message.error(error.message);
     },
-  })
-}
+  });
+};
 
 export const usePatchMenuMutation = () => {
-  const queryClient = useQueryClient()
-  const message = useMessage()
+  const queryClient = useQueryClient();
+  const message = useMessage();
 
   return useMutation({
     mutationFn: menuClient.patch,
-    onSuccess: () => {
-      message.success('Updated successfully')
+    onSuccess: (response) => {
+      message.success(response?.message ?? "Updated successfully");
 
       queryClient.invalidateQueries({
         queryKey: [API_ENDPOINTS.MENUS],
-      })
+      });
     },
     onError: (error: Error) => {
-      console.error('Update Menu failed:', error)
-      message.error(error.message)
+      message.error(error.message);
     },
-  })
-}
+  });
+};
 
 export const useDeleteMenuMutation = () => {
-  const queryClient = useQueryClient()
-  const message = useMessage()
+  const queryClient = useQueryClient();
+  const message = useMessage();
 
   return useMutation({
     mutationFn: menuClient.delete,
-    onSuccess: () => {
-      message.success('Deleted successfully')
+    onSuccess: (response) => {
+      message.success(response?.message ?? "Deleted successfully");
 
       queryClient.invalidateQueries({
         queryKey: [API_ENDPOINTS.MENUS],
-      })
+      });
     },
     onError: (error: Error) => {
-      console.error('Delete Menu failed:', error)
-      message.error(error.message)
+      message.error(error.message);
     },
-  })
-}
+  });
+};
